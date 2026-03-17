ab393aa5c2ccfc1739c69ce813a1299a5dc36b42
AvitalLugassi <avitall3560@gmail.com>
2026-03-17 17:08:10 +0200

    re-uploading astronaut_4  to fix transparency

--- java script/app.js
+++ java script/app.js
@@ -7,7 +7,7 @@ function createClients() {
   const dataClients = [
     ["teleskop", "images/Satellite.png", 1111],
     ["seklayev", "images/spaceship.png", 2222],
-    ["astronaut", "images/astronaut_3.png", 3333],
+    ["astronaut", "images/astronaut_4.png", 3333],
     ["apolo", "images/Apollo_Soyuz_3.png", 4444],
     ["shabtai", "images/Saturn_2.png", 5555],
   ];
